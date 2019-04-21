1da181c1e5ab571d8d5c313f55390f6813f698e6
caiyueliang <393900414@qq.com>
2019-04-21 19:25:14 +0800

    update

--- quantized.py
+++ quantized.py
@@ -5,11 +5,13 @@ import tensorflow as tf
 # 1.7-1.8	tf.contrib.lite.toco_convert
 
 
-def quantized(saved_model_dir):
+def quantized(saved_model_dir, export_model_name="./models/faceboxes_quant.tflite"):
+    # converter = tf.contrib.lite.Interpreter()
     converter = tf.contrib.lite.TocoConverter.from_saved_model(saved_model_dir)
     # converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
     converter.optimizations = [tf.contrib.lite.Optimize.OPTIMIZE_FOR_SIZE]
     tflite_quant_model = converter.convert()
+    open(export_model_name, "wb").write(tflite_quant_model)
 
 
 if __name__ == "__main__":
