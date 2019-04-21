47f4f509101318cc49da6aa70bc8ca5f89e93687
caiyueliang <393900414@qq.com>
2019-04-21 16:54:54 +0800

    update

--- quantized.py
+++ quantized.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 
+# 1.12	    tf.contrib.lite.TFLiteConverter
+# 1.9-1.11	tf.contrib.lite.TocoConverter
+# 1.7-1.8	tf.contrib.lite.toco_convert
+
 
 def quantized(saved_model_dir):
-    converter = tf.contrib.lite.TocoConverter()
+    converter = tf.contrib.lite.TocoConverter.from_saved_model(saved_model_dir)
     # converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
-    # converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_SIZE]
-    # tflite_quant_model = converter.convert()
+    converter.optimizations = [tf.contrib.lite.Optimize.OPTIMIZE_FOR_SIZE]
+    tflite_quant_model = converter.convert()
 
 
 if __name__ == "__main__":
